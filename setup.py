ccb93c6ad8208fd1d9f65a3ba211e25f2c08fb6e
vjrm <vjrmsg@gmail.com>
2021-05-18 16:15:52 -0400

    tox updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a Crash_Data package", 
+    author="VJ", 
+    packages=find_packages(),
+    license="MIT"
+)
